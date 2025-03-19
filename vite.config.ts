9976893a7a791ed301b5529ec9bf6e7d4f962a91
Muhammad Farrukh Anwar <muhammad.farrukh.anwar@outlook.com>
2025-03-20 01:24:31 +0200

    LR-4: Fix warnings and add aliases
    - locale import warning
    - alias support (QOL)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      '@Assets': `${path.resolve(__dirname, './src/assets')}`,
+      '@Components': `${path.resolve(__dirname, './src/components')}`,
+      '@Features': `${path.resolve(__dirname, './src/features')}`,
+      '@Routes': `${path.resolve(__dirname, './src/routes')}`,
+    },
+  },
+});
