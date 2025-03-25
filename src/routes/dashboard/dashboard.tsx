7dac6d880a7c4171a1c17771981100c9c924f707
Muhammad Farrukh Anwar <muhammad.farrukh.anwar@outlook.com>
2025-03-25 23:38:07 +0200

    LR-16: conclude efforts towards assignment

--- src/routes/dashboard/dashboard.tsx
+++ src/routes/dashboard/dashboard.tsx
@@ -34,10 +34,10 @@ export const Dashboard = (): React.ReactElement => {
           </Layout.Header>
           <Layout.Content style={{ boxSizing: 'border-box', padding: '24px' }}>
             <Row gutter={12}>
-              <Col span={14}>
+              <Col span={14} xs={24} lg={14} style={{ marginBottom: '12px' }}>
                 <Acquisitions />
               </Col>
-              <Col span={10}>
+              <Col span={10} xs={24} lg={10}>
                 <UserList />
               </Col>
             </Row>
